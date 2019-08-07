d10e5feab8254116d4e6b55c0cccc5032d85a266
Ron Green <11993626+georgettica@users.noreply.github.com>
2019-08-07 18:24:28 +0300

    make first json loading prototype

--- echo_bot.py
+++ echo_bot.py
@@ -4,65 +4,53 @@ import json
 import pickle
 import os 
 
-class User():
-    def __init__(self, user_name, user_id, user_status=u""):
-        self.user_name = user_name
-        self.user_id = user_id
-        self.user_status = user_status
-
-    def __eq__(self, other):
-        return self.user_id == other.user_id
-
-    def __hash__(self):
-        return self.user_id
-
-    def __str__(self):
-        return self.user_name.decode('UTF-8') + self.user_status.decode('UTF-8')
-
-    def set_status(self, user_status):
-        self.user_status = user_status
-
-
-def create_all_possible_possible_answers():
-    possible_answers = {}
-    allowed_answered = ['ביח', 'ביחידה', 'מחוץ', 'מח']
-    inside_messages = ['ביחידה', 'ביח', 'בדרך']
-    outside_messages = ['מחוץ', 'מחוץ ליחידה', 'מח']
-    sick_messages = ['מחלה']
-    vacation_messages = ['יום חופש', 'חופש']
-    dictionary = {'ביחידה': inside_messages, 'מחוץ ליחידה': outside_messages, 'יום מחלה': sick_messages, 'יום חופש': vacation_messages}
-    for status, texts in dictionary.items():
-        encoded_status = status.encode('UTF-8')
-        for text in texts:
-            possible_answers[text.encode('UTF-8')] = encoded_status
-    return possible_answers
+#class User():
+#    def __init__(self, user_name, user_id, user_status=u""):
+#        self.user_name = user_name
+#        self.user_id = user_id
+#        self.user_status = user_status
+#
+#    def __eq__(self, other):
+#        return self.user_id == other.user_id
+#
+#    def __hash__(self):
+#        return self.user_id
+#
+#    def __str__(self):
+#        return self.user_name.decode('UTF-8') + self.user_status.decode('UTF-8')
+#
+#    def set_status(self, user_status):
+#        self.user_status = user_status
 
 
-def is_new_user(message, users):
-    user_id = message.from_user.id
-    if user_id in users:
-        return False
-    else:
-        new_user = User(message.text[1:].encode('UTF-8'), user_id)
-        users[user_id] = new_user
-        with open('users.txt', 'wb') as handle:
-            pickle.dump(users, handle)
-        return True
+#def create_all_possible_possible_answers():
+#    possible_answers = {}
+#    for status, texts in dictionary.items():
+#        encoded_status = status.encode('UTF-8')
+#        for text in texts:
+#            possible_answers[text.encode('UTF-8')] = encoded_status
+#    return possible_answers
 
-def get_users():
-    with open('users.txt', 'rb') as handle:
-        try:
-            users =  pickle.loads(handle.read())
-            return users
-        except:
-            return {}
-         
 
-def get_bot():
-    return telebot.TeleBot("630906916:AAHMOb7KomCgbeetwQLnSZEyzPn9w0iYUrg")
-    # chat = bot.get_chat(-336818907)
-
-if __name__ == '__main__':
+#def is_new_user(message, users):
+#    user_id = message.from_user.id
+#    if user_id in users:
+#        return False
+#    else:
+#        new_user = User(message.text[1:].encode('UTF-8'), user_id)
+#        users[user_id] = new_user
+#        with open('users.txt', 'wb') as handle:
+#            pickle.dump(users, handle)
+#        return True
+#
+#def get_users():
+#    with open('users.txt', 'rb') as handle:
+#        try:
+#            users =  pickle.loads(handle.read())
+#            return users
+#        except:
+#            return {}
+def old():
     possible_answers = create_all_possible_possible_answers()
     users = get_users()
     bot = get_bot()
@@ -88,4 +76,17 @@ if __name__ == '__main__':
         else:
             bot.send_message(message.from_user.id, text = "I don't recognize what you wrote {} " .format(message.from_user.first_name))
     bot.polling()
-    
+
+def get_bot():
+    return telebot.TeleBot("630906916:AAHMOb7KomCgbeetwQLnSZEyzPn9w0iYUrg")
+    # chat = bot.get_chat(-336818907)
+
+def read_users_and_groups():
+    with open('./example_config.json','r') as f:
+        return json.load(f)
+
+def main():
+    json_string = read_users_and_groups()
+
+if __name__ == '__main__':
+   main() 
