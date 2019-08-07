657e7d385df1460af1b05b46ba7f338bd7b5df72
Ron Green <11993626+georgettica@users.noreply.github.com>
2019-08-07 13:12:08 +0300

    fix naming and add readability

--- echo_bot.py
+++ echo_bot.py
@@ -1,5 +1,7 @@
 import telebot
 import pprint
+import json
+
 bot = telebot.TeleBot("630906916:AAHMOb7KomCgbeetwQLnSZEyzPn9w0iYUrg")
 
 allowed_answered = [u'ביח', 'ביחידה', 'מחוץ', 'מח']
@@ -14,9 +16,11 @@ allowed_answered = [u'ביח', 'ביחידה', 'מחוץ', 'מח']
 def echo_all(message):
     parsed_text = message.text[::-1][:len(message.text)-1]
     print(parsed_text)
-    if parsed_text in allowed_answered:
-        bot.reply_to(message, "Got that {} " .format(message.from_user.first_name))
-    else:
-        bot.reply_to(message, "I don't recognize what you wrote {} " .format(message.from_user.first_name))
+    username = message.from_user.first_name
+
+    if parsed_text not in allowed_answered:
+        bot.reply_to(message, "I don't recognize what you wrote {} " .format(username))
+        return
+    bot.reply_to(message, "Got that {} " .format(username))
 
 bot.polling()
