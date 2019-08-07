6b4e013571c9fb4093f1e30a56ce98824a19e663
besdollma <47672215+besdollma@users.noreply.github.com>
2019-08-07 13:29:43 +0300

    additional changes

--- echo_bot.py
+++ echo_bot.py
@@ -1,26 +1,36 @@
 import telebot
 import pprint
-import json
-
 bot = telebot.TeleBot("630906916:AAHMOb7KomCgbeetwQLnSZEyzPn9w0iYUrg")
 
-allowed_answered = [u'ביח', 'ביחידה', 'מחוץ', 'מח']
+allowed_answered = ['ביח'.encode('UTF-8'), 'ביחידה', 'מחוץ', 'מח']
 
 
-# @bot.message_handler(commands=['start', 'help'])
-# def send_welcome(message):
-    # print(message)
-    # bot.reply_to(message, "Howdy, {} how are you doing?". format(message.from_user.first_name))
+inside_messages = ['ביחידה', 'ביח', 'בדרך']
+outside_messages = ['מחוץ', 'מחוץ ליחידה', 'מח']
+sick_messages = ['מחלה']
+vacation_messages = ['יום חופש', 'חופש']
 
-@bot.message_handler(func=lambda message: True)
+dictionary = {'ביחידה': inside_messages, 'מחוץ ליחידה': outside_messages, 'יום מחלה': sick_messages, 'יום חופש': vacation_messages}
+possible_answers = {}
+
+def create_all_possible_possible_answers():
+    for status, texts in dictionary.items():
+        encoded_status = status.encode('UTF-8')
+        for text in texts:
+            possible_answers[text.encode('UTF-8')] = encoded_status
+
+
+create_all_possible_possible_answers()
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_all(message):
-    parsed_text = message.text[::-1][:len(message.text)-1]
-    print(parsed_text)
-    username = message.from_user.first_name
+    parsed = message.text[1:].encode('UTF-8')
+    print(parsed)
+    # print(parsed_text)
+    if parsed in possible_answers:
+        bot.send_message(message.from_user.id, text = "Got that, adding your status as  {} " .format(possible_answers[parsed].decode('UTF-8')))
+    else:
+        bot.send_message(message.from_user.id, text = "I don't recognize what you wrote {} " .format(message.from_user.first_name))
 
-    if parsed_text not in allowed_answered:
-        bot.reply_to(message, "I don't recognize what you wrote {} " .format(username))
-        return
-    bot.reply_to(message, "Got that {} " .format(username))
 
 bot.polling()
