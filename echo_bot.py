974dd12f1c86feb50fb72db82294dbcd1681a355
besdollma <47672215+besdollma@users.noreply.github.com>
2019-08-07 12:48:44 +0300

    echo bot basics

--- echo_bot.py
+++ echo_bot.py
@@ -2,13 +2,21 @@ import telebot
 import pprint
 bot = telebot.TeleBot("630906916:AAHMOb7KomCgbeetwQLnSZEyzPn9w0iYUrg")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    print(message)
-    bot.reply_to(message, "Howdy, {} how are you doing?". format(message.text))
+allowed_answered = [u'ביח', 'ביחידה', 'מחוץ', 'מח']
+
+
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+    # print(message)
+    # bot.reply_to(message, "Howdy, {} how are you doing?". format(message.from_user.first_name))
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    parsed_text = message.text[::-1][:len(message.text)-1]
+    print(parsed_text)
+    if parsed_text in allowed_answered:
+        bot.reply_to(message, "Got that {} " .format(message.from_user.first_name))
+    else:
+        bot.reply_to(message, "I don't recognize what you wrote {} " .format(message.from_user.first_name))
 
 bot.polling()
