03e99e2eba7f124febc33993ec7ce99f57fc0f23
Jacob Levertov <jalevertov@gmail.com>
2019-08-07 20:27:34 +0300

    last commit before going home

--- modules/bot_manager.py
+++ modules/bot_manager.py
@@ -1,12 +1,13 @@
 import telebot
-from modules.json_manager import read_users_and_groups, initialize_group_counter
+from modules.json_manager import *
+from pprint import pprint
 
 class BotManager:
 
     def __init__(self):
         self.bot = telebot.TeleBot("630906916:AAHMOb7KomCgbeetwQLnSZEyzPn9w0iYUrg")
-        json_data = read_users_and_groups()
-        group_counters = initialize_group_counter(json_data)
+        self.json_data = read_users_and_groups()
+        self.group_counters = initialize(self.json_data)
 
         @self.bot.message_handler(commands=['stop'])
         def stop(self):
@@ -16,9 +17,11 @@ class BotManager:
         def echo_all(message):
             parsed_text = message.text[1:]
             splitted_text = parsed_text.split(':')
-            parsed_name, parsed_status = splitted_text[0], splitted_text[1]
-            print("PARSED_NAME::", parsed_name)
-            print("PARSED_STATUS::", parsed_status)        
+            parsed_name, parsed_status = splitted_text[0][::-1].strip(), splitted_text[1][::-1].strip()
+            update_user_status(parsed_name, parsed_status, self.json_data)
+            group_reported, group_name = is_group_reported(parsed_name, self.json_data) 
+            if group_reported:
+                self.bot.reply_to("Group {0} was fully reported".format(group_name))
 
     def poll(self):
         self.bot.polling()
--- modules/json_manager.py
+++ modules/json_manager.py
@@ -1,11 +1,58 @@
 import json
+from pprint import pprint
+
+name_to_group = {}
+groups_counters = {}
+
+database_path = "assets/database.json"
+
+
+def dump(json_data):
+    with open(database_path,'w',encoding='UTF-8') as f:
+        json.dump(json_data, f)
+
 
 def read_users_and_groups():
-    with open('assets/database.json','r') as f:
+    with open(database_path,'r') as f:
         return json.load(f)
 
-def initialize_group_counter(json):
-    groups_counters = {}
-    for group in json: 
-        groups_counters[group['name']] = 0
-    return groups_counters
+
+def initialize(json_data):
+    global groups_counters
+    for group in json_data.keys(): 
+        groups_counters[group[::-1]] = 0
+    map_name_to_group(json_data)
+    dump(json_data)
+
+
+def map_name_to_group(json_data):
+    global name_to_group
+    for group, people in json_data.items():
+        for person in people.keys():
+            name_to_group[person[::-1]] = group[::-1]
+
+
+def update_user_status(user, status, json_data):
+    print("USER::", user)
+    print("KEYS::", name_to_group.keys())
+    global groups_counters
+    if user not in name_to_group.keys():
+        raise IndexError
+    # TODO: validate status
+    group_name = name_to_group[user]
+    if json_data[group_name[::-1]][user[::-1]] == "":
+        groups_counters[group_name] += 1
+    json_data[group_name[::-1]][user[::-1]] = status
+    dump(json_data)
+
+def is_group_reported(user, json_data):
+    group_name = name_to_group[user]
+    return groups_counters[group_name] == len(json_data[group_name[::-1]]), group_name
+
+
+def clear_statuses(json_data):
+    for group, people in json_data.items():
+        for person, _ in people.items():
+            json_data[group][person]['status'] = ""
+    dump(json_data)
+        
